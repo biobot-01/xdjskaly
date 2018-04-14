0c4655b25981127b49374c19708e82b800c5f714
Kevin Michael <biobot.mike@gmail.com>
2018-04-14 03:02:15 +0200

    Initialize repo

---
+++ pelicanconf.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Kevin Michael'
+SITENAME = 'xDjSkaly'
+SITESUBTITLE = 'Sound and Music Production'
+SITEURL = ''
+
+CACHE_CONTENT = False
+LOAD_CONTENT_CACHE = False
+
+PATH = 'content'
+PAGE_PATHS = ['pages']
+THEME = 'theme/simplex'
+OUTPUT_PATH = 'output/'
+THEME_STATIC_DIR = 'theme/simplex'
+STATIC_EXCLUDES = ['pages']
+THEME_STATIC_PATHS = ['static', 'extra']
+DIRECT_TEMPLATES = ['index']
+
+TIMEZONE = 'Africa/Harare'
+LOCALE = ('en_ZA', 'C')
+
+DEFAULT_LANG = 'en-ZA'
+
+DISPLAY_PAGES_ON_MENU = False
+MENUITEMS = (
+    ('Home', '#home'),
+    ('About Me', '#about'),
+    ('Tracklist', '#music'),
+    ('Contact Me', '#contact')
+)
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Social widget
+SOUNDCLOUD_URL = 'https://soundcloud.com/xdjskaly'
+FACEBOOK_URL = 'https://www.facebook.com/xdjskaly'
+TWITTER_URL = 'https://twitter.com/xdjskaly'
+INSTAGRAM_URL = 'https://www.instagram.com/xdjskaly'
+YOUTUBE_URL = 'https://www.youtube.com/channel/UCG6XQCZc45R7yVyXdwFA7rQ'
+EMAIL = 'mailto:xdjskaly@gmail.com'
+SOCIAL_WIDGET_NAME = 'Social'
+SOCIAL = (
+    ('Soundcloud', SOUNDCLOUD_URL),
+    ('Facebook', FACEBOOK_URL),
+    ('Twitter', TWITTER_URL),
+    ('Instagram', INSTAGRAM_URL),
+    ('YouTube', YOUTUBE_URL),
+    ('Email', EMAIL)
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+INDEX_SAVE_AS = 'index.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
